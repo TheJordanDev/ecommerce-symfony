61f2613172022e4c02c5b5ca661c146ab33d59c1
TheJordanDev <thejordan.dev@gmail.com>
2022-12-15 08:33:28 +0100

    Fix: - Footer - Empty page error - Admin Lock for Product Crud

--- src/Controller/ProductsCrudController.php
+++ src/Controller/ProductsCrudController.php
@@ -6,11 +6,13 @@ use App\Entity\Products;
 use App\Form\ProductsType;
 use App\Repository\ProductsRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[IsGranted("ROLE_ADMIN")]
 #[Route('/crud/products')]
 class ProductsCrudController extends AbstractController
 {
